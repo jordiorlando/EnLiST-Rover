f23240f90ba951170724addaddd62c939b4b4840
Jordi Orlando <jordi.orlando@gmail.com>
2014-10-31 13:43:04 -0500

    Started implementing PID code

--- Code/Arduino/Motor Controller/Motor Controller.ino
+++ Code/Arduino/Motor Controller/Motor Controller.ino
@@ -1,7 +1,8 @@
 #include <Wire.h>
 #include <PID_v1.h>
 
-#define MOTOR_ADDRESS 0x42
+#define MOTOR_ADDRESS	0x42
+
 #define IN1		0
 #define IN2		1
 #define STALL	2
@@ -12,11 +13,13 @@
 #define QEA		9
 #define QEB		10
 
+int16_t nServo;
 double fQE, fPWM, fSpeed, P, I, D;
 PID speedPID(&fQE, &fPWM, &fSpeed, P, I, D, DIRECT);
 
 void setup()
 {
+	// connect to I2C with specified address
 	Wire.begin(MOTOR_ADDRESS);
 	Wire.onReceive(receiveEvent);
 	Wire.onRequest(requestEvent);
@@ -28,30 +31,88 @@ void setup()
 	// turn the PID controller on
 	speedPID.SetMode(AUTOMATIC);
 	speedPID.SetSampleTime(10);
+
+	pinMode(IN1, OUTPUT);
+	pinMode(IN2, OUTPUT);
+	pinMode(STALL, OUTPUT);
+	pinMode(STANDBY, OUTPUT);
+	pinMode(LAMP, OUTPUT);
+	pinMode(PWM, OUTPUT);
+	pinMode(SERVO, OUTPUT);
+	pinMode(QEA, INPUT);
+	pinMode(QEB, INPUT);
+
+	digitalWrite(STANDBY, HIGH);
 }
 
 void loop()
 {
 	fQE = 5;
 	speedPID.Compute();
-	analogWrite(PWM, fPWM);
+	analogWrite(PWM, (uint8_t)abs(fPWM));
 }
 
 void receiveEvent(int nNumBytes)
 {
 	uint8_t nBytes[nNumBytes];
-	for (uint8_t i = 0; i < nNumBytes; i++)
-	{
-		nBytes[i] = Wire.read();
 
-		if (nByte == 2)
-			digitalWrite(13, HIGH);
-		else if (nByte == 1)
-			digitalWrite(13, LOW);
+	for (uint8_t i = 0; i < nNumBytes; i++) {
+		nBytes[i] = Wire.read();
+	}
+
+	switch (nBytes[0])
+	{
+		case 0x00: // standby
+			digitalWrite(STANDBY, LOW);
+			fSpeed = 0;
+			break;
+		case 0x01: // freewheel
+			digitalWrite(IN1, LOW);
+			digitalWrite(IN2, LOW);
+			digitalWrite(STANDBY, HIGH);
+			fSpeed = 0;
+			break;
+		case 0x02: // brake
+			digitalWrite(IN1, HIGH);
+			digitalWrite(IN2, HIGH);
+			digitalWrite(STANDBY, HIGH);
+			fSpeed = 0;
+			break;
+		case 0x03: // turn forwards
+			digitalWrite(IN1, LOW);
+			digitalWrite(IN2, HIGH);
+			digitalWrite(STANDBY, HIGH);
+			fSpeed = (double)nBytes[1];
+			break;
+		case 0x04: // turn backwards
+			digitalWrite(IN1, HIGH);
+			digitalWrite(IN2, LOW);
+			digitalWrite(STANDBY, HIGH);
+			fSpeed = (double)nBytes[1] * -1;
+			break;
+		case 0x05: // set servo
+			nServo = nBytes[2];
+			nServo |= (nBytes[1] << 8);
+			break;
+		case 0x06: // turn off lamp
+			digitalWrite(LAMP, LOW);
+			break;
+		case 0x07: // turn on lamp
+			digitalWrite(LAMP, HIGH);
+			break;
 	}
 }
 
 void requestEvent()
 {
+	uint8_t nSpeed = ((int32_t)fSpeed >> 24) & 0x000000FF;
+	Wire.write(nSpeed);
+	nSpeed = ((int32_t)fSpeed >> 16) & 0x000000FF;
+	Wire.write(nSpeed);
+	nSpeed = ((int32_t)fSpeed >> 8) & 0x000000FF;
+	Wire.write(nSpeed);
+	nSpeed = (int32_t)fSpeed & 0x000000FF;
+	Wire.write(nSpeed);
+
 	Wire.write("hello ");
 }
